2384336c8141ce312c8f27822240a6a190f02c31
shuaibcs <alshuaibi7@gmail.com>
2019-12-02 17:31:40 +0100

    login basics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme} from './styles/theme'
+
+
+
 
 function App() {
   return (
     <div className="App">
-     
+      <ThemeProvider theme={theme}>
+      <Route exact path="/" component={Login} />
+    </ThemeProvider>
     </div>
   );
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,102 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { lighten } from 'material-ui/utils/colorManipulator';
+
+
+export const theme = createMuiTheme({
+  type: "dark",
+  palette: {
+    primary: {
+      light: "#FE4585",
+      main: "#FB1565",
+      dark: "#E5125C",
+      contrastText: "#070707"
+    },
+    secondary: {
+      light: "#ED9547",
+      main: "#ED8428",
+      dark: "#EF7912",
+      contrastText: "#070707"
+    },
+
+    textColor: "#E0E0E0",
+    backgroundColor: "#070707",
+  },
+  typography: {
+    useNextVariants: true,
+    fontFamily: "'Didact Gothic', sans-serif",
+
+    // THE BASE FONT SIZE IN HTML HAS TO BE SET TO 16px
+
+    h1: {
+      fontSize: "4rem",
+      fontWeight: "400",
+      lineHeight: "1.25",
+      fontFamily: "'Exo 2', sans-serif",
+    },
+    h2: {
+      fontSize: "3rem",
+      fontWeight: "600",
+      lineHeight: "1.25",
+      fontFamily: "'Exo 2', sans-serif",
+    },
+    h3: {
+      fontSize: "2.5rem",
+      fontWeight: "600",
+      lineHeight: "1.25",
+      fontFamily: "'Exo 2', sans-serif",
+    },
+    h4: {
+      fontSize: "2rem",
+      fontWeight: "800",
+      lineHeight: "1.25",
+    },
+    h5: {
+      fontSize: "1.5rem",
+      fontWeight: "600",
+      lineHeight: "1.20",
+      fontFamily: "'Exo 2', sans-serif",
+    },
+    h6: {
+      fontSize: "1rem",
+      fontWeight: "600",
+      color: "inherit",
+      lineHeight: "1.20",
+    },
+    body1: {
+      fontSize: "1rem",
+      fontWeight: "400",
+      lineHeight: "1.5",
+    },
+    body2: {
+      fontSize: "1rem",
+      fontWeight: "600",
+      lineHeight: "1.5",
+    },
+
+    button: {
+      fontSize: "1rem",
+      fontWeight: "600",
+      textTransform: "none",
+    },
+
+    caption: {
+      fontSize: "0.75rem",
+      fontWeight: "600",
+    },
+
+    overline: {
+      fontSize: "0.75rem",
+      fontWeight: "400",
+      textTransform: "none",
+    },
+  },
+  overrides: {
+    MuiOutlinedInput: {
+      root: {
+        "& > $notchedOutline": {
+          borderColor: lighten("#070707", 0.12), backgroundColor: lighten("#070707", 0.2)
+        }
+      }
+    }
+  }
+})
