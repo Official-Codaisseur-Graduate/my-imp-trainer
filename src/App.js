8e9cc5bdf57ff83bfa60e7d4b25c5a0e559b657c
apavlushina <ssemina7@gmail.com>
2019-12-03 16:47:19 +0100

    video component with styles exists

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login';
-import { ThemeProvider } from '@material-ui/core/styles';
-import { theme} from './styles/theme'
-
-
-
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Video from "./components/Video/Video";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./styles/theme";
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-      <Route exact path="/" component={Login} />
-    </ThemeProvider>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/video/:userId" component={Video} />
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,54 +1,51 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { withStyles } from '@material-ui/styles';
-import logo from '../images/logo.png'
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { withStyles } from "@material-ui/styles";
+import logo from "../images/logo.png";
 
 const useStyles = makeStyles(theme => ({
   paper: {
     paddingTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   logo: {
     width: "100%",
-    marginRight: '0',
+    marginRight: "0",
     marginBottom: "6%",
     marginTop: "1%",
 
     "& img": {
       width: "80%",
-    marginRight: '0',
-    marginBottom: "6%",
-    marginTop: "1%",
+      marginRight: "0",
+      marginBottom: "6%",
+      marginTop: "1%"
     }
-
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    margin: theme.spacing(3, 0, 2)
   },
   container: {
-    backgroundColor: theme.palette.backgroundColor,
+    backgroundColor: theme.palette.backgroundColor
   },
   textField: {
-    outlineColor: theme.palette.textColor,
-    
-  },
+    outlineColor: theme.palette.textColor
+  }
 }));
 
 //This is a test comment
@@ -56,18 +53,18 @@ export default function Login() {
   const classes = useStyles();
 
   return (
-    <Container  component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
         <div className={classes.logo}>
-         <img src={logo}/>
+          <img src={logo} alt="IMP Trainer" />
         </div>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
-          className={classes.textField}
+            className={classes.textField}
             variant="outlined"
             margin="normal"
             required
@@ -79,7 +76,7 @@ export default function Login() {
             autoFocus
           />
           <TextField
-          className={classes.textField}
+            className={classes.textField}
             variant="outlined"
             margin="normal"
             required
@@ -117,9 +114,7 @@ export default function Login() {
           </Grid>
         </form>
       </div>
-      <Box mt={8}>
-      
-      </Box>
+      <Box mt={8}></Box>
     </Container>
   );
 }
