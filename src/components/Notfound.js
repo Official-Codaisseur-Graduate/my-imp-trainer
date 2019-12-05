26f85511c6e71b98957c27c613404393a4f5dcea
shuaibcs <alshuaibi7@gmail.com>
2019-12-05 16:56:58 +0100

    404 addition features

--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -1,19 +1,48 @@
 
 import React from 'react';
 import '@lottiefiles/lottie-player';
+import  { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import logo from "../images/logo.png";
+
+
+const useStyles = makeStyles({
+  button: {
+    fontSize: "1rem",
+      fontWeight: "600",
+      textTransform: "none",
+      display:"block",
+  },  
+});
+
+
 
 
 export default function Notfound() {
- 
+  const classes = useStyles();
 
   const style={width: "100%",
    height: "100%"}
 
   return (
   <div>
-    
+    <div>
+          <img src={logo} style={{ width: "50vh" }} alt="logo" />
+        </div>
+
 <lottie-player
     src="https://assets3.lottiefiles.com/packages/lf20_552kX6.json" style={style} background="transparent"  speed="1"   loop  autoplay >
 </lottie-player>
+
+<Link to={'/dashboard'}>
+          <Button  className={classes.button}
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary" size="small">
+          Back to Dashboard
+        </Button>
+        </Link>
 </div>)
 }
